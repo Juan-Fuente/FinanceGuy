930f0965f71edc3b747b2bf2f450ec4b7913b00f
JD <focuswithmeaning@gmail.com>
2023-11-04 19:12:13 -0400

    second commit
    added array list

---
+++ ProjectCS/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCS
+{
+    internal class Class1
+    {
+        public string Food { get; set; }
+        public string Bills { get; set; }
+        public string Subscriptions { get; set; }
+        public string Outings { get; set; }
+
+    }
+}
---
+++ ProjectCS/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectCS
+{
+    internal class Class1
+    {
+
+        public static List<> foods = new List<>()
+            public static List<> foods = new List<>()
+            public static List<> foods = new List<>()
+            public static List<> foods = new List<>()
+
+
+
+    }
+}
