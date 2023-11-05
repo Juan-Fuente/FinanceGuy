f127fc02e2aa5d32d039ae064d5260239c77e10c
JD <focuswithmeaning@gmail.com>
2023-11-05 07:53:27 -0500

    tax calculator

--- ProjectCS/MainWindow.xaml.cs
+++ ProjectCS/MainWindow.xaml.cs
@@ -16,47 +16,73 @@ using System.Windows.Shapes;
 namespace ProjectCS
 {
 
-        public partial class MainWindow : Window
+    public partial class MainWindow : Window
     {
         public static List<Class1> cart = new List<Class1>();
 
         private decimal Total { get; set; }
-
-    
-
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+        private decimal Tax { get; set; }
 
 
-    private void foodSelect(object sender, selectFood f)
-    { 
-        if (Class2.bills.Any(x => x.Name == (string)mainBillsBox.SelectedItem))
-    {
-
-        var price = Items.fppds.Find(x => x.Name == (string)mainBillsBox.SelctedItem);
-        var item = itemsDataGrid.Items.IndexOf(price);
-        if (item >= 0)
+        public MainWindow()
         {
-            itemsDataGrid.Items.Remove(price);
-            price.Quantity++;
-            itemsDataGrid.Items.Add(Items.Bills.Find(x => x.Name == (string) mainCourseComboBox.SelectedItem));
-            
-            SalesTax(price);
-
-            mainBillsBox.SelectedIndex = -1;
+            InitializeComponent();
         }
-        else
+
+
+        // SelectionChangedEventArgs here
+
+        private void foodSelect(object sender, SelectionChangedEventArgs b)
         {
-            itemsDataGrid.Items.Add(Items.Bills.Find(x => x.Name == (string));
+            if (Class2.bills.Any(x => x.Name == (string)billsComboBox.SelectedItem))
+            {
 
-            SalesTax(price);
+                var price = Class2.foods.Find(x => x.Name == (string)billsComboBox.SelectedItem);
+                var item = itemsDataGrid.Items.IndexOf(price);
+                if (item >= 0)
+                {
+                    itemsDataGrid.Items.Remove(price);
+                    price.Quantity++;
+                    itemsDataGrid.Items.Add(Class2.foods.Find(x => x.Name == (string)billsComboBox.SelectedItem));
 
-            mainBillsBox.SelectedIndex = -1;
+                    SalesTax(price);
+
+                    billsComboBox.SelectedIndex = -1;
+                }
+                else
+                {
+                    itemsDataGrid.Items.Add(Class2.foods.Find(x => x.Name == (string)billsComboBox.SelectedItem));
+
+                    SalesTax(price);
+
+                    billsComboBox.SelectedIndex = -1;
+                }
+            }
         }
+
+        private void SalesTax(Class1 price)
+        {
+            decimal tax = 0.6M;
+            var salesTax = price.Price * tax;
+            salesTax = Math.Round(salesTax, 1);
+            if (salesTax < 0.1M)
+            {
+                salesTax = 0.1M;
+            }
+
+            Tax += salesTax;
+            taxBlock.Text = Tax.ToString("C2");
+
+            CalculateTotal(price, salesTax);
+        }
+        private void CalculateTotal(Class1 price, decimal salesTax)
+        {
+            var total = price.Price + salesTax;
+            Total += total;
+            totalTextBlock.Text = Total.ToString("C2");
+        }
+
     }
-}
         
         
         /// <summary>
