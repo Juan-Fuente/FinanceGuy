5bcab57ab5d3dcbc92d344ef7e7ddf64a007cd05
JD <focuswithmeaning@gmail.com>
2023-11-06 22:12:39 -0500

    google API integration

---
+++ ProjectCS/Window1.xaml.cs
@@ -0,0 +1,98 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjectCS
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        static string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+        static string ApplicationName = "Google Calendar API .NET Quickstart";
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void GoogleAPI()
+        {
+
+            UserCredential credential;
+            // Load client secrets.
+            using (var stream =
+                   new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                /* The file token.json stores the user's access and refresh tokens, and is created
+                 automatically when the authorization flow completes for the first time. */
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+
+            // Create Google Calendar API service.
+            var service = new CalendarService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName
+            });
+
+            // Define parameters of request.
+            EventsResource.ListRequest request = service.Events.List("primary");
+            request.TimeMin = DateTime.Now;
+            request.ShowDeleted = false;
+            request.SingleEvents = true;
+            request.MaxResults = 10;
+            request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+            // List events.
+            Events events = request.Execute();
+            Console.WriteLine("Upcoming events:");
+            if (events.Items == null || events.Items.Count == 0)
+            {
+                Console.WriteLine("No upcoming events found.");
+                return;
+            }
+            foreach (var eventItem in events.Items)
+            {
+                string when = eventItem.Start.DateTime.ToString();
+                if (String.IsNullOrEmpty(when))
+                {
+                    when = eventItem.Start.Date;
+                }
+                Console.WriteLine("{0} ({1})", eventItem.Summary, when);
+            }
+
+             catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+           
+    }
+}
+   
